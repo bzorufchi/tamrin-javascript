03742f535e4784a4b396dd71e921a6443cd36ba1
sia-smd <siavashsamadi@gmail.com>
2024-01-11 16:39:37 +0330

    end

--- random_function.js
+++ random_function.js
@@ -16,7 +16,7 @@ console.log('جای اشتباه',cunterp)
 console.log('صحیح',cuntert)
 
 function random(){
-    var x=Math.floor(Math.random() * 10);
+    var x=Math.floor(Math.random() * 10); 
     return x
 }
 function r(arr,test){
